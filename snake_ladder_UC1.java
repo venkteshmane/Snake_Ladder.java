17b5ec348693609096b76767a60e3ee5f2035f7c
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-02-02 14:29:01 +0530

    UC1

---
+++ snake_ladder_UC1.java
@@ -0,0 +1,13 @@
+package snakeladder;
+
+public class snake_ladder_UC1 {
+
+	public static void main(String[] args) {
+		int playerPosition = 0;
+	    System.out.println("Welcome to Snake Ladder Program");
+
+	    //Player Position Start With Zero
+	    System.out.println("Player Position:" + playerPosition);
+
+	    }
+	}
